08fa7a66cb8f1a7d969c7b546bebbc183448eaf3
Admin <Admin@TEB123012>
2022-10-21 09:07:36 +0200

    Add project files.

---
+++ Random uppgift 4/Random_uppgift_4.py
@@ -0,0 +1,12 @@
+import random
+
+udda=[1,3,5,7,9]
+jamna=[2,4,6,8,10]
+
+a=random.choice(udda)
+b=random.choice(jamna)
+
+print("Udda = ",udda)
+print("Jamna = ",jamna)
+print("Random udda + random jamna = ",a,"+",b,"=",a+b)
+print("Random udda * random jamna = ",a,"*",b,"=",a*b)
